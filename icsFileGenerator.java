fab56211b9efd6d1ac88c9905c45a2ef16ed314f
byogi <bapy@hawaii.edu>
2016-04-04 21:09:24 -1000

    Deliverable 3 reading in an ICS file
    Under the main method I started to add methods that can be used for
    deliverable 3.
    
    This one is the method that will allow a user to input any number of
    ICS files and have them put into an arrayList. Should be able to pretty
    much copy and paste the code into any of our stuff in order to use it.

--- icsFileGenerator.java
+++ icsFileGenerator.java
@@ -5,6 +5,7 @@
 
 import java.io.*;
 import java.util.*;
+import java.io.FileNotFoundException;//added for deliverable 3
 
 public class icsFileGenerator 
 {
@@ -88,5 +89,59 @@ public class icsFileGenerator
 		classificationInput.close();//close scanner
 		
 	}
+	
+	//---Method(s) for deliverable 3 start here---//
+	
+	
+	
+	//This Method will allow user to input any number of ICS Files 
+	public static void inputIcsFile(ArrayList<File> files) throws IOException, FileNotFoundException
+	{
+		//below is the set up for variables we'll use//
+		int totalNumFiles = 0;//the integer value of the number of files user plans to enter
+		int currIndex = 0;//what current index of the arrayList we're on
+		String icsFile;//will hold the name of the current ICS file name the user wishes to enter
+		Scanner userInput = new Scanner (System.in);//For user input  when prompted
+		
+		//prompting the user for input is below
+		System.out.println("How many ICS file would would you like to enter?");
+		totalNumFiles = userInput.nextInt();//sets the users total number of ics files
+		
+		//below is the loop that will be putting the ICS files into our array list
+		while (totalNumFiles > currIndex)
+		{
+			System.out.println("What is the name of the next file? Please be sure to type it as it appears on your computer: ");
+			icsFile = userInput.next();
+			
+			try 
+			{
+				files.add(new File (icsFile));//add file to the arrayList
+			}
+			
+			finally
+			{
+				currIndex ++;//after try block executes currIndex will be increased by a value of 1
+			}
+			
+		}
+		
+		
+		userInput.close();//close the scanner after loop completes
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
